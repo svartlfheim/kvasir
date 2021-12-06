f5541ef5e4f3cf44f24e8f6aa4984373c58ebde3
Adam Kirk <adam_kirk@live.co.uk>
2021-12-06 17:13:06 +0000

    Initialise the symfony app
    Initialised a base symfony app in the api directory. We'll be using
    a multi-tiered architecture (standalone frontend app communicating
    with backend api), and we'll be using a mono-repo structure for
    keeping everything in one place.
    
    It is a base symfony installation with a new controller in the
    ./src/Connections/Controller/... directory which is a simple
    hello-world endpoint, which we can use to ensure our docker
    environment is working.

---
+++ api/src/Connections/Controller/API/V1/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Connections\Controller\API\V1;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class DefaultController
+{
+
+	#[Route('', name: 'list')]
+	public function index(): JsonResponse 
+	{
+		return new JsonResponse(['hello' => 'world']);
+	}
+}
